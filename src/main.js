0c2593f592c9c27661b9990d948b1547af06b25f
HarrisonHG <yesitisalloneword@gmail.com>
2025-06-19 17:34:06 +0100

    chore: :rocket: Hello world!

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// entry point for future logic — right now just verify you loaded it
+console.log("🎉 Dawn sim loaded");
+const app = document.getElementById("app");
+app.textContent = "hello dawn!";
+
